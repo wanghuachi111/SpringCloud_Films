71e3f417ff534f39634495886737a2d195da1392
83946 <839462980@qq.com>
2021-05-15 20:25:27 +0800

    2021.05.15.2nd

--- backend_film/src/main/java/com/mooc/meetingfilm/film/service/FilmServiceImpl.java
+++ backend_film/src/main/java/com/mooc/meetingfilm/film/service/FilmServiceImpl.java
@@ -1,4 +1,5 @@
 package com.mooc.meetingfilm.film.service;
+import java.time.LocalDateTime;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -6,11 +7,18 @@ import com.mooc.meetingfilm.film.controller.vo.DescribeActorsRespVO;
 import com.mooc.meetingfilm.film.controller.vo.DescribeFilmRespVO;
 import com.mooc.meetingfilm.film.controller.vo.DescribeSingleFilmRespVO;
 import com.mooc.meetingfilm.film.controller.vo.FilmSavedReqVO;
+import com.mooc.meetingfilm.film.dao.entity.MoocActorT;
+import com.mooc.meetingfilm.film.dao.entity.MoocFilmActorT;
+import com.mooc.meetingfilm.film.dao.entity.MoocFilmInfoT;
+import com.mooc.meetingfilm.film.dao.entity.MoocFilmT;
 import com.mooc.meetingfilm.film.dao.mapper.MoocActorTMapper;
 import com.mooc.meetingfilm.film.dao.mapper.MoocFilmActorTMapper;
 import com.mooc.meetingfilm.film.dao.mapper.MoocFilmInfoTMapper;
 import com.mooc.meetingfilm.film.dao.mapper.MoocFilmTMapper;
 import com.mooc.meetingfilm.utils.common.exception.CommonServiceException;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +33,7 @@ import javax.annotation.Resource;
  * @Description: 影片模块实现层
  * @date 2021/5/1219:18
  */
+@Data
 @Service
 public class FilmServiceImpl implements FilmServiceAPI {
 
@@ -92,6 +101,43 @@ public class FilmServiceImpl implements FilmServiceAPI {
     @Transactional(rollbackFor = Exception.class)
     public void saveFilm(FilmSavedReqVO filmSavedReqVO) throws CommonServiceException {
         try {
+            //保存电影主表
+            MoocFilmT filmT = new MoocFilmT();
+            filmT.setFilmName("");
+            filmT.setFilmType(0);
+            filmT.setImgAddress("");
+            filmT.setFilmScore("");
+            filmT.setFilmPresalenum(0);
+            filmT.setFilmBoxOffice(0);
+            filmT.setFilmSource(0);
+            filmT.setFilmCats("");
+            filmT.setFilmArea(0);
+            filmT.setFilmDate(0);
+            filmT.setFilmTime(LocalDateTime.now());
+            filmT.setFilmStatus(0);
+
+            filmTMapper.insert(filmT);
+
+            //保存电影子表
+            MoocFilmInfoT filmInfoT = new MoocFilmInfoT();
+            filmInfoT.setFilmId(filmT.getUuid() + "");
+            filmInfoT.setFilmEnName("");
+            filmInfoT.setFilmScore("");
+            filmInfoT.setFilmScoreNum(0);
+            filmInfoT.setFilmLength(0);
+            filmInfoT.setBiography("");
+            filmInfoT.setDirectorId(0);
+            filmInfoT.setFilmImgs("");
+
+            filmInfoTMapper.insert(filmInfoT);
+
+            //保存演员映射表
+            MoocFilmActorT filmActorT = new MoocFilmActorT();
+            filmActorTMapper.insert(filmActorT);
+            filmActorT.setFilmId(filmT.getUuid());
+            filmActorT.setActorId(0);
+            filmActorT.setRoleName("");
+
 
         } catch (Exception e) {
             throw new CommonServiceException(500, e.getMessage());
